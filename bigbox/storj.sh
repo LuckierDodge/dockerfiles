5229969bc446bdea99ee25c6bcf61903017b10ff
Ryan Lewis <ryandlewis.rl@gmail.com>
2021-01-07 19:58:34 +0000

    Updated storj script

--- bigbox/storj.sh
+++ bigbox/storj.sh
@@ -2,11 +2,11 @@
 
 docker run -d --restart unless-stopped --stop-timeout 300 \
 	-p 28967:28967 \
-	-p 127.0.0.1:14002:14002 \
-	-e WALLET="0xXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" \
-	-e EMAIL="ryandlewis.rl@gmail.com" \
-	-e ADDRESS="storj.ryandlewis.dev:28967" \
-	-e STORAGE="1TB" \
+	-p 14002:14002 \
+	-e WALLET="" \
+	-e EMAIL="" \
+	-e ADDRESS="" \
+	-e STORAGE="" \
 	--mount type=bind,source="/home/luckierdodge/.local/share/storj/identity/storagenode",destination=/app/identity \
 	--mount type=bind,source="/home/luckierdodge/storj",destination=/app/config \
 	--name storagenode storjlabs/storagenode:latest
